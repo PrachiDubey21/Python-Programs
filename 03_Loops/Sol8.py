72c1e4989723232479ab146f2a4c7d6d5fba58b2
Prachi Dubey <006prachi@gmail.com>
2026-06-27 10:48:21 +0530

    Loop Question

--- 03_Loops/Sol8.py
+++ 03_Loops/Sol8.py
@@ -1,15 +1,15 @@
-num = int(input("Enter a number: "))
+number = int(input("Enter a number: "))
 isPrime = True
 
-if num <= 1:
+if number <= 1:
     isPrime = False
 else:
-    for i in range(2, num):
-        if num % i == 0:
+    for i in range(2, number):
+        if number % i == 0:
             isPrime = False
             break
 
 if isPrime:
-    print(num, "is a prime number")
+    print(number, "is a prime number")
 else:
-    print(num, "is not a prime number")
+    print(number, "is not a prime number")
